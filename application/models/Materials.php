ad3c623397caf840271f7d3d99e2c58167efbdaa
hoza <khaled.hoza@gmail.com>
2016-04-15 13:39:37 +0200

    first commit

---
+++ application/models/Materials.php
@@ -0,0 +1,9 @@
+<?php
+
+class Application_Model_Materials extends Zend_Db_Table_Abstract
+{
+	/** Table name */
+	protected $_name= 'materials' ;
+
+}
+
---
+++ application/models/Users.php
@@ -0,0 +1,9 @@
+<?php
+
+class Application_Model_Users extends Zend_Db_Table_Abstract
+{
+	/** Table name */
+	protected $_name= 'users' ;
+
+}
+
