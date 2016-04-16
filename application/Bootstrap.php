269525e656571e16dd04570a797c5d96acdcd4de
ahmed sobhy <eng_ahmed9393@yahoo.com>
2016-04-16 17:44:18 +0200

    Login, Registration and Layout Design with bootstrap >> Done

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,6 +3,31 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
+//include files css and js to loaded auto matically ////
+	protected function _initPlaceholders()
+	{
+		$this->bootstrap('View');
+		$view = $this->getResource('View');
+		$view->doctype('XHTML1_STRICT');
+		//add Meta tags
+		$view->headMeta()->appendName('keywords', 'blog')->appendHttpEquiv('Content-Type','text/html;charset=utf-8');
+		// Set the initial title and separator:
+		$view->headTitle('Et3lm ')->setSeparator(' :: ');
+		// Set the initial stylesheet:
+		$view->headLink()->prependStylesheet('/css/site.css');
+		// Set the initial JS to load:
+		$view->headScript()->prependFile('/js/site.js');
+	}
+
+	//To activate session
+	protected function _initSession(){
+		//to start session////
+		Zend_Session::start();
+		//create namespace into session
+		$session = new Zend_Session_Namespace( 'Zend_Auth' );
+		$session->setExpirationSeconds( 1800 );
+		// $session->x=5;
+	}
 
 }
 
--- application/models/Users.php
+++ application/models/Users.php
@@ -5,5 +5,21 @@ class Application_Model_Users extends Zend_Db_Table_Abstract
 	/** Table name */
 	protected $_name= 'users' ;
 
+	function register($data){	
+
+		if(isset($data['controller'])) 
+			unset($data['controller']);
+		if(isset($data['action'])) 
+			unset($data['action']);
+		if(isset($data['module'])) 
+			unset($data['module']);
+		if(isset($data['Submit'])) 
+			unset($data['Submit']);
+		if(isset($data['MAX_FILE_SIZE'])) 
+			unset($data['MAX_FILE_SIZE']);
+		
+		return $this->insert($data);
+	}
+
 }
 
