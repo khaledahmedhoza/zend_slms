6cc15053175d7c05f5cf0717384006155e454f9b
Zahra Meki <zahrameki18@gmail.com>
2016-04-17 17:02:03 +0200

    design

--- application/controllers/CourseController.php
+++ application/controllers/CourseController.php
@@ -1,34 +1,27 @@
 <?php
-
-class UsersController extends Zend_Controller_Action
+class CourseController extends Zend_Controller_Action
 {
+
 	private $user_model;
-	//private request_model;
 	private $material_model;
 	private $course_model;
 	private $comment_model;
 	private $category_model;
 
-    public function init()
-    {
-        /* Initialize action controller here */
+
+
+public function init()
+{
+	/* Initialize action controller here */
 	$this->user_model = new Application_Model_Users;
-	//$this->request_model = new Application_Model_Requests;
 	$this->material_model = new Application_Model_Materials;
 	$this->course_model = new Application_Model_Courses;
 	$this->comment_model = new Application_Model_Comments;
 	$this->category_model = new Application_Model_Categories;
-    }
+}
 
-    public function indexAction()
-    {
-        // action body
-    }
-
-
+public function courseAction()
 
 
 }
-
-
-
+//$this->view->users = $this->model->listUsers();
--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -2,10 +2,22 @@
 
 class IndexController extends Zend_Controller_Action
 {
+		private $user_model;
+		//private request_model;
+		private $material_model;
+		private $course_model;
+		private $comment_model;
+		private $category_model;
 
     public function init()
     {
         /* Initialize action controller here */
+        	$this->user_model = new Application_Model_Users;
+			//$this->request_model = new Application_Model_Requests;
+			$this->material_model = new Application_Model_Materials;
+			$this->course_model = new Application_Model_Courses;
+			$this->comment_model = new Application_Model_Comments;
+			$this->category_model = new Application_Model_Categories;
     }
 
     public function indexAction()
@@ -13,6 +25,13 @@ class IndexController extends Zend_Controller_Action
         // action body
     }
 
+    public function homeAction()
+    {
+       	$this->view->index = $this->course_model->listCourses();
+
+       //	'data' => $this->listCourses()->fetchAll(),
+
+    }
 
 }
 
